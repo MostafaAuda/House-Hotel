630b94d578d6c474b62f31b645e8cccab2176584
Mostafa Auda <mostafa.auda@outlook.com>
2019-11-10 22:40:36 +0200

    final touches

--- js/main.js
+++ js/main.js
@@ -117,4 +117,22 @@ $(document).ready(function() {
   });
   
   //--------------------------------------------------------------------------------------------------//
+
+  var btn = $('#backToTop');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, 2000);
+  });
+
+  //--------------------------------------------------------------------------------------------------//
+
 });
